7ef6c136d7e858e6b3f7023beb3f50de00a3c838
Mariana Garcia <marianagarcia901974@gmail.com>
2020-12-17 16:36:36 -0300

    criando método de instanciação

--- src/main/java/com/udemy/projeto/ProjetoApplication.java
+++ src/main/java/com/udemy/projeto/ProjetoApplication.java
@@ -1,13 +1,30 @@
 package com.udemy.projeto;
 
+import com.udemy.projeto.domain.Category;
+import com.udemy.projeto.repositoies.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
+//CommandLineRunner permite adicionar mais um método para auxiliar o programa
 @SpringBootApplication
-public class ProjetoApplication {
+public class ProjetoApplication implements CommandLineRunner {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(ProjetoApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        Category cat1 = new Category(null, "Informática");
+        Category cat2 = new Category(null, "Escritório");
+
+        categoryRepository.saveAll(Arrays.asList(cat1,cat2));
+    }
 }
