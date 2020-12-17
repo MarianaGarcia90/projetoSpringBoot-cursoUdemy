7e8852fc60ab7d69fb343f1c30ffefc467edd689
Mariana Garcia <marianagarcia901974@gmail.com>
2020-12-17 15:19:52 -0300

    configurando h2 e alteração da Category para criar tab

--- src/main/java/com/udemy/projeto/domain/Category.java
+++ src/main/java/com/udemy/projeto/domain/Category.java
@@ -1,11 +1,17 @@
 package com.udemy.projeto.domain;
 
-import lombok.Data;
-
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 
+@Entity
 public class Category implements Serializable {
      private  static final long serialVersionUID = 1l;
+
+     @Id
+     @GeneratedValue(strategy = GenerationType.IDENTITY)
      private Integer id;
      private String name;
 
