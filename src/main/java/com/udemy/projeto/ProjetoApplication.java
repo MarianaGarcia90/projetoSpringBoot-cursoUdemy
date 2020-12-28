42a69f4a26491705fada27776fbb0d20c063821f
Mariana Garcia <marianagarcia901974@gmail.com>
2020-12-28 15:04:49 -0300

    Criando profile de dev

--- src/main/java/com/udemy/projeto/ProjetoApplication.java
+++ src/main/java/com/udemy/projeto/ProjetoApplication.java
@@ -1,16 +1,9 @@
 package com.udemy.projeto;
 
-import com.udemy.projeto.model.*;
-import com.udemy.projeto.model.enums.CostumerType;
-import com.udemy.projeto.model.enums.PaymentState;
-import com.udemy.projeto.repositoies.*;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
 
 //CommandLineRunner permite adicionar mais um método para auxiliar o programa
 @SpringBootApplication
